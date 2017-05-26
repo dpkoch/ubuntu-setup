3f9d68f7bcce969a25ca9819b5e415e0f3620900
Daniel Koch <daniel.koch@byu.edu>
2017-05-25 22:22:22 -0600

    Added texlive-humanities package

--- scripts/install_latex.sh
+++ scripts/install_latex.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo apt-get -y install texlive texlive-latex-extra texlive-bibtex-extra texlive-fonts-extra texlive-math-extra texlive-extra-utils texlive-science biber latexmk
+sudo apt-get -y install texlive texlive-latex-extra texlive-bibtex-extra texlive-fonts-extra texlive-math-extra texlive-extra-utils texlive-science texlive-humanities biber latexmk
 
 if [ ! -d "/home/$USER/.local/bin" ]; then
   mkdir /home/$USER/.local/bin
