c9d7e266989ee2db0f672f0ff38fb281b870c806
Daniel Koch <daniel.koch@byu.edu>
2017-06-01 17:36:49 -0600

    Added some git configuration

--- scripts/install_misc.sh
+++ scripts/install_misc.sh
@@ -5,6 +5,9 @@ sudo apt-get -y install git
 sudo apt-get -y install vim
 sudo apt-get -y install gimp
 sudo apt-get -y install inkscape
+sudo apt-get -y install meld
 
 # configuration
 git config --global core.editor vim
+git config --global diff.tool meld
+git config --global merge.tool meld
