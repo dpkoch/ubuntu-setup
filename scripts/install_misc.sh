fff4d243bf8a5b991d8f3d41b0582b28df16e0e9
Daniel Koch <daniel.koch@byu.edu>
2017-06-14 12:37:40 -0600

    Added astyle install

--- scripts/install_misc.sh
+++ scripts/install_misc.sh
@@ -6,6 +6,7 @@ sudo apt-get -y install vim
 sudo apt-get -y install gimp
 sudo apt-get -y install inkscape
 sudo apt-get -y install meld
+sudo apt-get -y install astyle
 
 # configuration
 git config --global core.editor vim
