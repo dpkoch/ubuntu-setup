8b2f1604a02941c527923834da54b6fa689bd9ec
Daniel Koch <daniel.koch@byu.edu>
2017-07-12 15:11:35 -0600

    Added vim configuration

--- scripts/install_misc.sh
+++ scripts/install_misc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # install packages
 sudo apt-get -y install git
 sudo apt-get -y install vim
@@ -9,11 +11,14 @@ sudo apt-get -y install meld
 sudo apt-get -y install astyle
 sudo apt-get -y install screen
 
-# configuration
+# git configuration
 git config --global push.default simple
 git config --global core.editor vim
 git config --global diff.tool meld
 git config --global merge.tool meld
+
+# vim configuration
+cp $DIR/../resources/vimrc ~/.vimrc
 
 # mkdocs
 sudo apt-get -y install python-pip
