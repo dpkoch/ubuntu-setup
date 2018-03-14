dce92024fc36c092c8f8e06f219beb2da4b7c9db
Daniel Koch <daniel.koch@byu.edu>
2018-03-14 12:00:29 -0600

    Added materia and paper themes to install

--- scripts/install_themes.sh
+++ scripts/install_themes.sh
@@ -4,6 +4,8 @@
 sudo add-apt-repository ppa:tista/adapta # adapta theme
 sudo add-apt-repository ppa:noobslab/themes # arc theme
 sudo add-apt-repository ppa:numix/ppa # numix themes
+sudo add-apt-repository ppa:dyatlov-igor/materia-theme # materia theme
+sudo add-apt-repository ppa:snwh/pulp # paper theme
 
 sudo apt update
 
@@ -18,3 +20,9 @@ sudo apt -y install arc-theme
 
 # numix icon themes
 sudo apt -y install numix-icon-theme numix-icon-theme-square numix-icon-theme-circle numix-gtk-theme
+
+# materia theme
+sudo apt -y install materia-theme
+
+# paper themes
+sudo apt -y install paper-gtk-theme paper-icon-theme paper-cursor-theme
