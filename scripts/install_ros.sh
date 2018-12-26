7129ae5205ca94aeee5833feb98a1b151fa40ebe
Daniel Koch <daniel.p.koch@gmail.com>
2018-12-26 14:01:28 -0700

    Updated ROS installation to melodic

--- scripts/install_ros.sh
+++ scripts/install_ros.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# install (from http://wiki.ros.org/kinetic/Installation/Ubuntu)
+# install (from http://wiki.ros.org/melodic/Installation/Ubuntu)
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get update
-sudo apt-get -y install ros-kinetic-desktop-full
+sudo apt update
+sudo apt -y install ros-melodic-desktop-full
 sudo rosdep init
 rosdep update
 sudo apt-get -y install python-rosinstall
@@ -18,13 +18,7 @@ fi
 cp $DIR/../resources/setrosmaster ~/.local/bin/
 
 cp $DIR/../resources/rosconsole ~/.rosconsole
-
-echo "export ROS_MASTER_URI=http://\`cat ~/.rosmasteruri\`:11311" > ~/.rosrc
-echo "" >> ~/.rosrc
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.rosrc
-echo "" >> ~/.rosrc
-echo "export ROSCONSOLE_FORMAT='[\${severity}] [\${node}]: \${message}'" >> ~/.rosrc
-echo "export ROSCONSOLE_CONFIG_FILE=/home/$USER/.rosconsole" >> ~/.rosrc
+cp $DIR/../resources/rosrc ~/.rosrc
 
 if ! grep -xq "source /home/$USER/.rosrc" ~/.bashrc
 then
