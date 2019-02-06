ae5d8c8a9c1ec814c7f7116348ac56ba8be1cc35
Daniel Koch <daniel.p.koch@gmail.com>
2019-02-06 10:07:42 -0700

    Fixes to i3 install script

--- scripts/install_i3.sh
+++ scripts/install_i3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # icon theme
-sudo add-apt-repository ppa:numix/ppa # numix themes
+sudo add-apt-repository -y ppa:numix/ppa # numix themes
 sudo apt update
 sudo apt -y install numix-icon-theme-circle
 
@@ -11,10 +11,13 @@ sudo apt -y install i3
 # rofi
 sudo apt -y install rofi
 
+# feh
+sudo apt -y install feh
+
 # termite
 # source: https://computingforgeeks.com/install-termite-terminal-on-ubuntu-18-04-ubuntu-16-04-lts/
-sudo apt install build-essential
-sudo apt-get install -y git g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac intltool libpcre2-dev libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev libxml2-utils gperf
+sudo apt -y install build-essential
+sudo apt -y install git g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac intltool libpcre2-dev libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev libxml2-utils gperf
 
 pushd . &> /dev/null
 cd $(mktemp -d)
@@ -34,15 +37,20 @@ sudo mkdir -p /lib/terminfo/x
 sudo ln -s /usr/local/share/terminfo/x/xterm-termite /lib/terminfo/x/xterm-termite
 sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/termite 60
 
+popd &> /dev/null
+
 # audio management
-sudo apt -y install pulseaudio-utils playerctl
+sudo apt -y install pulseaudio-utils
 
 # copy configuration files
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp -r $DIR/../resources/i3 ~
+cp -r $DIR/../resources/i3/. ~
 
 # don't start graphical service by default
 #sudo systemctl enable multi-user.target --force
 sudo systemctl set-default multi-user.target
 
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo "To finish installation, manually install playerctl from the .deb file"
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 
