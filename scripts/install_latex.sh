2c62d410eac0c6b3352654f5d838b8ddf82b5cca
Daniel Koch <daniel.p.koch@gmail.com>
2019-02-13 12:07:16 -0700

    Updated LaTeX install script

--- scripts/install_latex.sh
+++ scripts/install_latex.sh
@@ -2,11 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo apt-get -y install texlive texlive-latex-extra texlive-generic-extra texlive-bibtex-extra texlive-fonts-extra texlive-math-extra texlive-extra-utils texlive-science texlive-humanities biber latexmk
+sudo apt -y install texlive texlive-latex-extra texlive-generic-extra texlive-bibtex-extra texlive-fonts-extra texlive-extra-utils texlive-science texlive-humanities biber latexmk
 
 if [ ! -d "/home/$USER/.local/bin" ]; then
   mkdir /home/$USER/.local/bin
 fi
 cp $DIR/../resources/clean-latex ~/.local/bin/
 
-sudo apt-get -y install okular
+sudo apt -y install okular
