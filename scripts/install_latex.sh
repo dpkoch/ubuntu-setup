fca2145661cb62200324ee7f5868fe62ec5432a6
Daniel Koch <daniel.p.koch@gmail.com>
2019-03-12 18:17:50 -0600

    Install additional latex package

--- scripts/install_latex.sh
+++ scripts/install_latex.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo apt -y install texlive texlive-latex-extra texlive-generic-extra texlive-bibtex-extra texlive-fonts-extra texlive-extra-utils texlive-science texlive-humanities biber latexmk
+sudo apt -y install texlive texlive-latex-extra texlive-generic-extra texlive-bibtex-extra texlive-fonts-extra texlive-extra-utils texlive-science texlive-humanities texlive-font-utils biber latexmk
 
 if [ ! -d "/home/$USER/.local/bin" ]; then
   mkdir /home/$USER/.local/bin
